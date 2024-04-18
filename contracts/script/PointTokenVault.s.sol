84fab212290b83fcd490af2f8dff26c9fa5217bd
Josh Levine <joshuapark95@gmail.com>
2024-04-18 04:39:01 -0400

    chore: add v basic script

--- contracts/script/PointTokenVault.s.sol
+++ contracts/script/PointTokenVault.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import {Script} from "forge-std/Script.sol";
 
-import {PointTokenVault, PointTokenHub} from "../PointTokenVault.sol";
+import {PointTokenVault} from "../PointTokenVault.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
@@ -17,36 +17,27 @@ contract DeployPointTokenSystem is Script {
     uint256 SAM_PRIVATE_KEY = 0x8563bad6b0b906b890cd3272ee8748b7d0e20d6e49917e769af364598e96b466;
     uint256 AVA_PRIVATE_KEY = 0x7617580e9556785c7f9bb93e652df98b6acd0de459300711afbcf53e40ce0358;
 
-    address public SEOPLIA_SAFE_ADDRESS = 0xec48011b60be299A2684F36Bdb3B498a61A6CbF3;
+    address public SEOPLIA_MERKLE_BOT_SAFE = 0xec48011b60be299A2684F36Bdb3B498a61A6CbF3;
+    address public SEOPLIA_ADMIN_SAFE = 0xec48011b60be299A2684F36Bdb3B498a61A6CbF3; // todo: change to actual admin safe
 
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY_MERKLE_BOT_A");
         vm.startBroadcast(deployerPrivateKey);
-
-        // MockERC20 token = MockERC20(0x758d92D1ade72Bce33c5677a23208280c259F00A);
-        // PointTokenHub pointTokenHub = PointTokenHub(0x81bDb5C8fedEfeeee890607aB49D64bb2fa94edb);
-        // PointTokenVault pointTokenVault = PointTokenVault(0x8Ff06724e15B6ACcAeD6164DE2ab9FA15c4980ad);
-
-        // token.balanceOf(SAM);
 
         // token.approve(address(pointTokenVault), 5e18);
         // pointTokenVault.deposit(token, 5e18, SAM);
 
         // token.balanceOf(SAM);
 
-        // PointTokenHub PTHubSingleton = new PointTokenHub();
-        // PointTokenVault PTVSingleton = new PointTokenVault();
+        PointTokenVault PTVSingleton = new PointTokenVault();
 
         // TODO: use create two for deterministic addresses across chains
+        PointTokenVault pointTokenVault = PointTokenVault(
+            address(new ERC1967Proxy(address(PTVSingleton), abi.encodeCall(PointTokenVault.initialize, ())))
+        );
 
-        // PointTokenHub pointTokenHub = PointTokenHub(
-        //     address(new ERC1967Proxy(address(PTHubSingleton), abi.encodeCall(PointTokenHub.initialize, ())))
-        // );
-        // PointTokenVault pointTokenVault = PointTokenVault(
-        //     address(
-        //         new ERC1967Proxy(address(PTVSingleton), abi.encodeCall(PointTokenVault.initialize, (pointTokenHub)))
-        //     )
-        // );
+        pointTokenVault.grantRole(pointTokenVault.MERKLE_UPDATER_ROLE(), SEOPLIA_MERKLE_BOT_SAFE);
+        pointTokenVault.grantRole(pointTokenVault.DEFAULT_ADMIN_ROLE(), SEOPLIA_ADMIN_SAFE);
 
         // pointTokenVault.upgradeToAndCall(address(PTVSingleton), bytes(""));
 
