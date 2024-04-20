d6b3ef98e9a2c61105d77d7d8505f839c8c72d11
Josh Levine <joshuapark95@gmail.com>
2024-04-20 19:24:04 -0400

    feat: add gnosis safe queuing to deployment scripts

--- contracts/script/PointTokenVault.s.sol
+++ contracts/script/PointTokenVault.s.sol
@@ -5,10 +5,10 @@ import {Script} from "forge-std/Script.sol";
 
 import {PointTokenVault} from "../PointTokenVault.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
-
 import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
+import {BatchScript} from "forge-safe/src/BatchScript.sol";
 
-contract DeployPointTokenSystem is Script {
+contract DeployPointTokenSystem is Script, BatchScript {
     // Sepolia Test Accounts
     address payable public JIM = payable(0xD6633c1382896079D3576eC43519d844a8C80B56);
     address payable public SAM = payable(0xeeD5B3026060218Dc270AE672be6468053e65E39);
@@ -25,11 +25,6 @@ contract DeployPointTokenSystem is Script {
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-
-        // token.approve(address(pointTokenVault), 5e18);
-        // pointTokenVault.deposit(token, 5e18, SAM);
-
-        // token.balanceOf(SAM);
 
         PointTokenVault PointTokenVaultImplementation = new PointTokenVault();
 
@@ -48,19 +43,23 @@ contract DeployPointTokenSystem is Script {
         vm.stopBroadcast();
     }
 
-    function deposit() {
+    function deposit() public {
         vm.startBroadcast(JIM_PRIVATE_KEY);
+
+        ERC20 token = ERC20(0x8Ff06724e15B6ACcAeD6164DE2ab9FA15c4980ad);
 
         token.approve(address(pointTokenVault), 5e18);
         pointTokenVault.deposit(token, 5e18, JIM);
 
-        token.balanceOf(JIM);
-
         vm.stopBroadcast();
+
+        return token.balanceOf(JIM);
     }
 
-    function upgrade() {
+    function upgrade() public {
         vm.startBroadcast();
+
+        PointTokenVault currentPointTokenVault = PointTokenVault(0x8Ff06724e15B6ACcAeD6164DE2ab9FA15c4980ad);
 
         PointTokenVault PointTokenVaultImplementation = new PointTokenVault();
 
@@ -69,13 +68,30 @@ contract DeployPointTokenSystem is Script {
         vm.stopBroadcast();
     }
 
-    function deployMockERC20() {
+    function deployMockERC20() public {
         vm.startBroadcast(JIM_PRIVATE_KEY);
 
         MockERC20 token = new MockERC20("Test Token", "TST", 18);
 
         token.mint(JIM, 100e18);
+        token.mint(SAM, 100e18);
+        token.mint(AVA, 100e18);
 
+        vm.stopBroadcast();
+    }
+
+    function setCap() public {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        bytes32 root = 0x1230000000000000000000000000000000000000000000000000000000000000;
+
+        address pointTokenVault = 0x8Ff06724e15B6ACcAeD6164DE2ab9FA15c4980ad;
+
+        bytes memory txn = abi.encodeWithSelector(PointTokenVault.updateRoot.selector, root);
+        addToBatch(pointTokenVault, 0, txn);
+
+        executeBatch(SEOPLIA_ADMIN_SAFE, true);
         vm.stopBroadcast();
     }
 }
