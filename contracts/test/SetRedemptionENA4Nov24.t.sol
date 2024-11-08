3618514435a8781a3fbc82068149f658bbe6b120
Steven Valeri <steven@sense.finance>
2024-11-07 22:12:26 -0500

    feat: two additional tests expecting revert for claim

--- contracts/test/SetRedemptionENA4Nov24.t.sol
+++ contracts/test/SetRedemptionENA4Nov24.t.sol
@@ -36,6 +36,39 @@ contract SetRedemptionENA4Nov24Test is Test {
         vaultV0_1_0.redeemRewards(PointTokenVault.Claim(pointsId, AMOUNT, AMOUNT, proof), USER);
     }
 
+    function test_FailedRedemptionRights1_BadProof() public {
+        bytes32[] memory proof = new bytes32[](5);
+        proof[0] = 0x000008976022911aeb40e40bcb5754f9529f2080710f7ec1db8ace85c4f7b7f8;
+        proof[1] = 0xc6f15a7cbd986873c6b761e81c98ee8ac4afd1c9885b7d8e0ae4de752040ab12;
+        proof[2] = 0xca808b743099c608cd9b81872c528c521d76505c116bedabfdcc6307c9c92bfb;
+        proof[3] = 0x703f21e968e8791afb70bcf780821f479ea90632b109016d8b24c8637771383c;
+        proof[4] = 0x0f76084b6c6777c64b0f591ee64d8c66c54c0bdeb5ce44142823c0f74b856267;
+
+        address USER = 0x25E426b153e74Ab36b2685c3A464272De60888Ae;
+        uint256 AMOUNT = 52792622186481736164;
+
+        vm.prank(USER);
+        vm.expectRevert(PointTokenVault.ProofInvalidOrExpired.selector);
+        vaultV0_1_0.redeemRewards(PointTokenVault.Claim(pointsId, AMOUNT, AMOUNT, proof), USER);
+    }
+
+    function test_RedemptionRights1_ClaimTooMuch() public {
+        bytes32[] memory proof = new bytes32[](5);
+        proof[0] = 0x991fd8976022911aeb40e40bcb5754f9529f2080710f7ec1db8ace85c4f7b7f8;
+        proof[1] = 0xc6f15a7cbd986873c6b761e81c98ee8ac4afd1c9885b7d8e0ae4de752040ab12;
+        proof[2] = 0xca808b743099c608cd9b81872c528c521d76505c116bedabfdcc6307c9c92bfb;
+        proof[3] = 0x703f21e968e8791afb70bcf780821f479ea90632b109016d8b24c8637771383c;
+        proof[4] = 0x0f76084b6c6777c64b0f591ee64d8c66c54c0bdeb5ce44142823c0f74b856267;
+
+        address USER = 0x25E426b153e74Ab36b2685c3A464272De60888Ae;
+        uint256 TOTAL_CLAIMABLE = 52792622186481736164;
+        uint256 CLAIM_AMOUNT = 52792622186481736164 + 10;
+
+        vm.prank(USER);
+        vm.expectRevert(PointTokenVault.ClaimTooLarge.selector);
+        vaultV0_1_0.redeemRewards(PointTokenVault.Claim(pointsId, TOTAL_CLAIMABLE, CLAIM_AMOUNT, proof), USER);
+    }
+
     function test_NormalPTokenClaim() public {
         bytes32[] memory proof = new bytes32[](5);
         proof[0] = 0x8b788cab342842ae529d8efac9659e1af9367270c13caaf4c8b5ef4c67a51402;
